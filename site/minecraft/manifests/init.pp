d0e64813ada508767b25fe8f7013274b23f275ca
Beza G. Tassew <getbez@gmail.com>
2023-06-12 10:33:45 +0200

    extract variables

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,15 +1,18 @@
 class minecraft {
+  $url = 'https://piston-data.mojang.com/v1/objects/15c777e2cfe0556eef19aab534b186c0c6f277e1/server.jar'
+  $install_dir = 'opt/minecraft'
   file {'/opt/minecraft':
     ensure => directory
   }
-  file {'/opt/minecraft/server.jar':
+  file {"${install_dir}/server.jar"}:
     ensure => file,
-    source => 'https://piston-data.mojang.com/v1/objects/15c777e2cfe0556eef19aab534b186c0c6f277e1/server.jar',
+    source => $url,
+    before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  file {"${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true'
   }
@@ -20,5 +23,6 @@ class minecraft {
   service{'minecraft':
     ensure => running,
     enable => true,
+    require => [Package['java'], File["${install_dir}/eula.txt'], File['puppet:///modules/minecraft/minecraft.service']],
   }
 }
